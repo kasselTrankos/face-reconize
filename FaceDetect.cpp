c744a1cc7b0575a4d544f45d82879bcacf0efd22
alvaro <alvaro.touzon@gmail.com>
2019-01-08 22:17:43 +0100

    add haars files

--- FaceDetect.cpp
+++ FaceDetect.cpp
@@ -23,13 +23,16 @@ int main(int argc, char *argv[])
   char* imageName = argv[1];
   cout << "Loading input image: " << imageName << endl;
   Mat img;
-  img = imread(imageName, IMREAD_COLOR);
+  img = imread(imageName, COLOR_BGR2GRAY);
 
     CascadeClassifier cascade;
-    if (cascade.load("haarcascade_frontalface_alt.xml")) {
+    CascadeClassifier profileface;
+    CascadeClassifier eyes;
+    if (cascade.load("haarcascade_frontalface_default.xml")
+      && eyes.load("haarcascade_eye.xml")) {
       cout << " FOLO" << endl;
       vector<Rect> faces;
-      cascade.detectMultiScale(img, faces, 1.1, 3, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+      cascade.detectMultiScale(img, faces, 1.3, 5);
       printf("%zd face(s) are found.\n", faces.size());
       for( vector<cv::Rect>::const_iterator r = faces.begin(); r != faces.end(); r++) {            
         //rectangle(img, *r, Scalar(0,0,255), 2, 8, 0);
@@ -38,12 +41,16 @@ int main(int argc, char *argv[])
           Point( r->x + r->width, r->y + r->height ), 
           Scalar(0,0,255));
         string m = "rect-"  + string(imageName);
+        // eyes = eyes.detectMultiScale(roi_gray)
+        // for( vector<cv::Rect>::const_iterator r = faces.begin(); r != faces.end(); r++) {
+        //   cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2);
+        // }
         imwrite(m, img);
       }
     }
-    namedWindow( "Image with Rects", WINDOW_NORMAL );// Create a window for display.
-    imshow( "Faces", img );                   // Show our image inside it.
+    namedWindow( "Faces", WINDOW_NORMAL );// Create a window for display.
+    imshow( "Faces", img );               // Show our image inside it.
 
-    waitKey(0);                                          // Wait for a keystroke in the window
+    waitKey(0);                           // Wait for a keystroke in the window
     return 0;
 }
